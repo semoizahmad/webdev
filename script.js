e6d5cbf16c890c0ed6779f8793d9b0cc9ee4de98
semoizahmad <ma5263264@gmail.com>
2026-09-16 21:49:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// yahan tumhare original code ka poora JavaScript
+// config
+// loader
+// cursor
+// navigation
+// reveal animations
+// tilt effects
+// magnetic buttons
+// project filters
+// tabs
+// cart
+// WhatsApp checkout
+// etc.
